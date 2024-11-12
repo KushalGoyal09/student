9e286a721844c8c5c966da1a971c5cb98293c8c2
Kushal Goyal <116098036+KushalGoyal09@users.noreply.github.com>
2024-11-12 22:01:02 +0530

    fix

--- client/src/pages/MentorSalary.tsx
+++ client/src/pages/MentorSalary.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {useState, useEffect} from "react";
 import {
   Select,
@@ -44,8 +46,8 @@ export default function SalaryManagement() {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [activeTab, setActiveTab] = useState<Role>("GroupMentor");
   const [mentors, setMentors] = useState<{
-    GroupMentor: (GroupMentor | SeniorMentor)[];
-    SeniorMentor: (GroupMentor | SeniorMentor)[];
+    GroupMentor: GroupMentor[];
+    SeniorMentor: GroupMentor[];
     Employee: Employee[];
   }>({
     GroupMentor: [],
@@ -63,10 +65,11 @@ export default function SalaryManagement() {
   });
   const [commonSalary, setCommonSalary] = useState<CommonSalary | null>(null);
   const [editingId, setEditingId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false); // Added loading state
 
   useEffect(() => {
     fetchData();
-  }, [selectedMonth, selectedYear]);
+  }, [selectedMonth, selectedYear, activeTab]);
 
   const fetchData = async () => {
     const [gm, sm, emp] = await Promise.all([
@@ -126,6 +129,7 @@ export default function SalaryManagement() {
   };
 
   const handleEditCommonSalary = async () => {
+    setIsLoading(true); // Set loading state to true
     if (commonSalary) {
       await setCommonSalaryDetails(
         activeTab,
@@ -135,7 +139,39 @@ export default function SalaryManagement() {
         commonSalary.payAccordingToRating,
         commonSalary.baseSalary,
       );
+      await updateAllSalaries();
     }
+    setIsLoading(false);
+  };
+
+  const updateAllSalaries = async () => {
+    const promises = mentors[activeTab].map((mentor) => {
+      const salary = salaries[activeTab].find(
+        (s) => s.userId === mentor.id,
+      ) || {
+        userId: mentor.id,
+        totalSalary:
+          activeTab !== "Employee" && commonSalary
+            ? calculateTotalSalary(
+                mentor as GroupMentor | SeniorMentor,
+                commonSalary,
+              )
+            : 0,
+        bonus: 0,
+        paid: false,
+      };
+      return editSalary(
+        selectedMonth,
+        selectedYear,
+        mentor.id,
+        salary.totalSalary,
+        salary.bonus,
+        salary.paid,
+        activeTab,
+      );
+    });
+    await Promise.all(promises);
+    fetchData();
   };
 
   const calculateTotalSalary = (
@@ -158,6 +194,17 @@ export default function SalaryManagement() {
   const totalToPay = salaries[activeTab]
     .filter((salary) => !salary.paid)
     .reduce((sum, salary) => sum + salary.totalSalary + salary.bonus, 0);
+
+  const handleSalaryChange = (
+    userId: string,
+    field: "totalSalary" | "bonus",
+    value: number,
+  ) => {
+    const updatedSalaries = salaries[activeTab].map((salary) =>
+      salary.userId === userId ? {...salary, [field]: value} : salary,
+    );
+    setSalaries({...salaries, [activeTab]: updatedSalaries});
+  };
 
   return (
     <div className="container mx-auto px-4 py-6 sm:p-6 bg-gray-50 min-h-screen">
@@ -229,119 +276,127 @@ export default function SalaryManagement() {
               </CardTitle>
             </CardHeader>
             <CardContent className="p-6">
-              {commonSalary && activeTab !== "Employee" && (
-                <div className="mb-6 p-6 border rounded-lg shadow-sm bg-gray-50">
-                  <h3 className="text-xl font-semibold mb-4 text-pcb">
-                    Common Pay Settings
-                  </h3>
-                  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6">
-                    <div className="flex flex-col">
-                      <Label htmlFor="basePay" className="mb-2 text-pcb">
-                        BASE PAY
-                      </Label>
-                      <Input
-                        type="number"
-                        id="basePay"
-                        value={commonSalary.baseSalary || ""}
-                        onChange={(e) =>
-                          setCommonSalary({
-                            ...commonSalary,
-                            baseSalary: Number(e.target.value),
-                          })
-                        }
-                        placeholder="Base Pay"
-                        className="w-full border-pcb"
-                      />
-                    </div>
-                    {!commonSalary.payAccordingToRating && (
+              {commonSalary &&
+                (activeTab === "GroupMentor" ||
+                  activeTab === "SeniorMentor") && (
+                  <div className="mb-6 p-6 border rounded-lg shadow-sm bg-gray-50">
+                    <h3 className="text-xl font-semibold mb-4 text-pcb">
+                      Common Pay Settings
+                    </h3>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6">
                       <div className="flex flex-col">
-                        <Label htmlFor="perAj" className="mb-2 text-pcb">
-                          PER AJ
+                        <Label htmlFor="basePay" className="mb-2 text-pcb">
+                          BASE PAY
                         </Label>
                         <Input
                           type="number"
-                          id="perAj"
-                          value={commonSalary.perAj || ""}
+                          id="basePay"
+                          value={commonSalary.baseSalary || ""}
                           onChange={(e) =>
                             setCommonSalary({
                               ...commonSalary,
-                              perAj: Number(e.target.value),
+                              baseSalary: Number(e.target.value),
                             })
                           }
-                          placeholder="Per AJ"
+                          placeholder="Base Pay"
                           className="w-full border-pcb"
                         />
                       </div>
-                    )}
-                    <div className="flex items-center space-x-2 col-span-full">
-                      <input
-                        type="checkbox"
-                        id="payAsPerRating"
-                        checked={commonSalary.payAccordingToRating}
-                        onChange={(e) =>
-                          setCommonSalary({
-                            ...commonSalary,
-                            payAccordingToRating: e.target.checked,
-                          })
-                        }
-                        className="form-checkbox h-5 w-5 text-pcb"
-                      />
-                      <Label
-                        htmlFor="payAsPerRating"
-                        className="cursor-pointer text-pcb"
-                      >
-                        Pay As Per Rating
-                      </Label>
+                      {!commonSalary.payAccordingToRating && (
+                        <div className="flex flex-col">
+                          <Label htmlFor="perAj" className="mb-2 text-pcb">
+                            PER AJ
+                          </Label>
+                          <Input
+                            type="number"
+                            id="perAj"
+                            value={commonSalary.perAj || ""}
+                            onChange={(e) =>
+                              setCommonSalary({
+                                ...commonSalary,
+                                perAj: Number(e.target.value),
+                              })
+                            }
+                            placeholder="Per AJ"
+                            className="w-full border-pcb"
+                          />
+                        </div>
+                      )}
+                      <div className="flex items-center space-x-2 col-span-full">
+                        <input
+                          type="checkbox"
+                          id="payAsPerRating"
+                          checked={commonSalary.payAccordingToRating}
+                          onChange={(e) =>
+                            setCommonSalary({
+                              ...commonSalary,
+                              payAccordingToRating: e.target.checked,
+                            })
+                          }
+                          className="form-checkbox h-5 w-5 text-pcb"
+                        />
+                        <Label
+                          htmlFor="payAsPerRating"
+                          className="cursor-pointer text-pcb"
+                        >
+                          Pay As Per Rating
+                        </Label>
+                      </div>
+                      {commonSalary.payAccordingToRating && (
+                        <>
+                          <div className="flex flex-col">
+                            <Label
+                              htmlFor="perAjLess"
+                              className="mb-2 text-pcb"
+                            >
+                              PER AJ (rating &lt; 4.5)
+                            </Label>
+                            <Input
+                              type="number"
+                              id="perAjLess"
+                              value={commonSalary.perAjLess || ""}
+                              onChange={(e) =>
+                                setCommonSalary({
+                                  ...commonSalary,
+                                  perAjLess: Number(e.target.value),
+                                })
+                              }
+                              placeholder="Pay for Less than 4.5 rating"
+                              className="w-full border-pcb"
+                            />
+                          </div>
+                          <div className="flex flex-col">
+                            <Label
+                              htmlFor="perAjMore"
+                              className="mb-2 text-pcb"
+                            >
+                              PER AJ (rating &gt; 4.5)
+                            </Label>
+                            <Input
+                              type="number"
+                              id="perAjMore"
+                              value={commonSalary.perAjMore || ""}
+                              onChange={(e) =>
+                                setCommonSalary({
+                                  ...commonSalary,
+                                  perAjMore: Number(e.target.value),
+                                })
+                              }
+                              placeholder="Pay for More than 4.5 rating"
+                              className="w-full border-pcb"
+                            />
+                          </div>
+                        </>
+                      )}
                     </div>
-                    {commonSalary.payAccordingToRating && (
-                      <>
-                        <div className="flex flex-col">
-                          <Label htmlFor="perAjLess" className="mb-2 text-pcb">
-                            PER AJ (rating &lt; 4.5)
-                          </Label>
-                          <Input
-                            type="number"
-                            id="perAjLess"
-                            value={commonSalary.perAjLess || ""}
-                            onChange={(e) =>
-                              setCommonSalary({
-                                ...commonSalary,
-                                perAjLess: Number(e.target.value),
-                              })
-                            }
-                            placeholder="Pay for Less than 4.5 rating"
-                            className="w-full border-pcb"
-                          />
-                        </div>
-                        <div className="flex flex-col">
-                          <Label htmlFor="perAjMore" className="mb-2 text-pcb">
-                            PER AJ (rating &gt; 4.5)
-                          </Label>
-                          <Input
-                            type="number"
-                            id="perAjMore"
-                            value={commonSalary.perAjMore || ""}
-                            onChange={(e) =>
-                              setCommonSalary({
-                                ...commonSalary,
-                                perAjMore: Number(e.target.value),
-                              })
-                            }
-                            placeholder="Pay for More than 4.5 rating"
-                            className="w-full border-pcb"
-                          />
-                        </div>
-                      </>
-                    )}
+                    <Button
+                      onClick={handleEditCommonSalary}
+                      className="mt-6 w-full sm:w-auto bg-pcb hover:bg-pcb/90 text-white p-2 rounded"
+                    >
+                      {isLoading ? "Saving..." : "Save Common Settings"}
+                    </Button>
                   </div>
-                  <Button
-                    onClick={handleEditCommonSalary}
-                    className="mt-6 w-full sm:w-auto bg-pcb hover:bg-pcb/90 text-white p-2 rounded"
-                  >
-                    Save Common Settings
-                  </Button>
-                </div>
-              )}
+                )}
               <div className="overflow-x-auto relative">
                 <div className="min-w-[800px]">
                   <Table>
@@ -420,22 +475,13 @@ export default function SalaryManagement() {
                               <Input
                                 type="number"
                                 value={salary.totalSalary || ""}
-                                onChange={(e) => {
-                                  const updatedSalaries = salaries[
-                                    activeTab
-                                  ].map((s) =>
-                                    s.userId === mentor.id
-                                      ? {
-                                          ...s,
-                                          totalSalary: Number(e.target.value),
-                                        }
-                                      : s,
-                                  );
-                                  setSalaries({
-                                    ...salaries,
-                                    [activeTab]: updatedSalaries,
-                                  });
-                                }}
+                                onChange={(e) =>
+                                  handleSalaryChange(
+                                    mentor.id,
+                                    "totalSalary",
+                                    Number(e.target.value),
+                                  )
+                                }
                                 readOnly={!isEditing}
                                 className="w-full border-pcb"
                               />
@@ -444,22 +490,13 @@ export default function SalaryManagement() {
                               <Input
                                 type="number"
                                 value={salary.bonus || ""}
-                                onChange={(e) => {
-                                  const updatedSalaries = salaries[
-                                    activeTab
-                                  ].map((s) =>
-                                    s.userId === mentor.id
-                                      ? {
-                                          ...s,
-                                          bonus: Number(e.target.value),
-                                        }
-                                      : s,
-                                  );
-                                  setSalaries({
-                                    ...salaries,
-                                    [activeTab]: updatedSalaries,
-                                  });
-                                }}
+                                onChange={(e) =>
+                                  handleSalaryChange(
+                                    mentor.id,
+                                    "bonus",
+                                    Number(e.target.value),
+                                  )
+                                }
                                 readOnly={!isEditing}
                                 className="w-full border-pcb"
                               />
